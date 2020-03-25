9581bd1c92292ac0e44284d20f4c98347431239c
vinhdk <anlalayker@gmail.com>
2020-03-25 18:44:38 +0700

    5 files modified

--- src/app/core/core.routing.ts
+++ src/app/core/core.routing.ts
@@ -4,27 +4,27 @@ const routes: Routes = [
   {
     path: "",
     component: LayoutComponent,
-    children: [
-      {
-        path: "",
-        redirectTo: "order"
-      },
-      {
-        path: "order",
-        loadChildren: () =>
-          import("./order/order.module").then(mod => mod.OrderModule)
-      },
-      {
-        path: "service",
-        loadChildren: () =>
-          import("./service/service.module").then(mod => mod.ServiceModule)
-      },
-      {
-        path: "user",
-        loadChildren: () =>
-          import("./user/user.module").then(mod => mod.UserModule)
-      }
-    ]
+    // children: [
+    //   {
+    //     path: "",
+    //     redirectTo: "order"
+    //   },
+    //   {
+    //     path: "order",
+    //     loadChildren: () =>
+    //       import("./order/order.module").then(mod => mod.OrderModule)
+    //   },
+    //   {
+    //     path: "service",
+    //     loadChildren: () =>
+    //       import("./service/service.module").then(mod => mod.ServiceModule)
+    //   },
+    //   {
+    //     path: "user",
+    //     loadChildren: () =>
+    //       import("./user/user.module").then(mod => mod.UserModule)
+    //   }
+    // ]
   }
 ];
 
