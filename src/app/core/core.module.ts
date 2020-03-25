e6a3c69faa20a2da67765e8d5ff08393869a475d
vinhdk <anlalayker@gmail.com>
2020-03-25 18:35:07 +0700

    1 file modified

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreRoutes } from './core.routing';
 import { LayoutComponent } from './layout/layout.component';
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 @NgModule({
   imports: [
     CommonModule,
-    CoreRoutes
+    CoreRoutes,
   ],
   declarations: [LayoutComponent]
 })
